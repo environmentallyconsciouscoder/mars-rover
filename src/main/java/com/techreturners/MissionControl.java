3da1303e93dc81708ee557311f9388580c006591
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-09-05 16:51:12 +0100

    added testMaxVehicleCount

--- src/main/java/com/techreturners/MissionControl.java
+++ src/main/java/com/techreturners/MissionControl.java
@@ -2,19 +2,23 @@ package com.techreturners;
 
 public class MissionControl {
 
-    private static final int MAX_VEHICLE_COUNT = 7;
+    private static final int MAX_VEHICLE_COUNT = 3;
     private static int vehicleCount = 0;
 
     public void deployVehicle(String type, String model) {
-        if (vehicleCount <= MAX_VEHICLE_COUNT) {
+        if (vehicleCount < MAX_VEHICLE_COUNT) {
             new Vehicle(type, model);
             vehicleCount += 1;
         } else {
-            throw new IllegalStateException("Maximum number of vehicles deployed reached");
+//            throw new IllegalStateException("Maximum number of vehicles deployed reached");
         }
     }
 
     public int getVehicleCount() {
         return vehicleCount;
+    }
+
+    public int getMaxVehicleCount() {
+        return MAX_VEHICLE_COUNT;
     }
 }
--- src/test/java/MissionControlTest.java
+++ src/test/java/MissionControlTest.java
@@ -1,4 +1,5 @@
 import com.techreturners.MissionControl;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -11,9 +12,34 @@ public class MissionControlTest {
     public void testDeployVehicle(String vehicleType, String model, String score) {
 
         MissionControl missionControl = new MissionControl();
-        missionControl.deployVehicle(vehicleType, model);
 
-        assertEquals(Integer.parseInt(score), missionControl.getVehicleCount());
+//        System.out.println(missionControl.getVehicleCount());
+//        System.out.println(missionControl.getMaxVehicleCount());
+        boolean isNoMoreThanMaxVehicleCount = missionControl.getVehicleCount() < missionControl.getMaxVehicleCount();
+//        System.out.println(isNoMoreThanMaxVehicleCount);
+
+        if (isNoMoreThanMaxVehicleCount) {
+            missionControl.deployVehicle(vehicleType, model);
+//            System.out.println(missionControl.getVehicleCount());
+//            System.out.println(Integer.parseInt(score));
+
+            assertEquals(Integer.parseInt(score), missionControl.getVehicleCount());
+        }
+
+    }
+
+    @Test
+    public void testMaxVehicleCount() {
+
+        MissionControl missionControl = new MissionControl();
+
+//        System.out.println(missionControl.getVehicleCount());
+//        System.out.println(missionControl.getMaxVehicleCount());
+
+        boolean isNoMoreThanMaxVehicleCount = missionControl.getVehicleCount() <= missionControl.getMaxVehicleCount();
+//        System.out.println(isNoMoreThanMaxVehicleCount);
+
+        assertEquals(true, isNoMoreThanMaxVehicleCount);
     }
 
     //create a plateau
