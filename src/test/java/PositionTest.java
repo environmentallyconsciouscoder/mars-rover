d0eb10b300b200983e81b4a71fa1c49701de610f
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-09-07 23:53:50 +0100

    added test case for position

---
+++ src/test/java/PositionTest.java
@@ -0,0 +1,22 @@
+import com.techreturners.Position;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PositionTest {
+
+    @Test
+    public void testPosition() {
+
+        int xValue = 1;
+        int yValue = 2;
+        char cardinalDirection = 'N';
+
+        Position position = new Position(xValue, yValue, cardinalDirection);
+
+        assertEquals(yValue, position.getYValue());
+        assertEquals(xValue, position.getXValue());
+        assertEquals(cardinalDirection, position.getCardinalDirection());
+
+    }
+}
