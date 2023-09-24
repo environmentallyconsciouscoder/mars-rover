24266c389f6c01f1e79b558c1bfd3baa4c9a6bc9
James Tang <133535682+environmentallyconsciouscoder@users.noreply.github.com>
2023-09-24 20:52:38 +0100

    in the plateau class I removed static variables and methods. In the class vehicle, I inherit plateau class and use the super() to constuct a plateau

--- src/main/java/com/techreturners/Plateau.java
+++ src/main/java/com/techreturners/Plateau.java
@@ -1,10 +1,15 @@
 package com.techreturners;
 
 public class Plateau {
-    private static int minX = 0;
-    private static int maxX = 5;
-    private static int minY = 0;
-    private static int maxY = 5;
+//    private static int minX = 0;
+//    private static int maxX = 5;
+//    private static int minY = 0;
+//    private static int maxY = 5;
+
+    private int minX = 0;
+    private int maxX = 5;
+    private int minY = 0;
+    private int maxY = 5;
 
     public Plateau(int minX, int maxX, int minY, int maxY) {
         this.minX = minX;
@@ -14,19 +19,35 @@ public class Plateau {
         this.maxY = maxY;
     }
 
-    public static int getMinX() {
+//    public static int getMinX() {
+//        return minX;
+//    }
+//
+//    public static int getMaxX() {
+//        return maxX;
+//    }
+//
+//    public static int getMinY() {
+//        return minY;
+//    }
+//
+//    public static int getMaxY() {
+//        return maxY;
+//    }
+
+    public int getMinX() {
         return minX;
     }
 
-    public static int getMaxX() {
+    public int getMaxX() {
         return maxX;
     }
 
-    public static int getMinY() {
+    public int getMinY() {
         return minY;
     }
 
-    public static int getMaxY() {
+    public int getMaxY() {
         return maxY;
     }
 }
